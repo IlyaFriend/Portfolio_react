c78b851aaa4d6f42bebb432a206260b67d8ca7de
IlyaFriend <eliasbell2233@gmail.com>
2023-11-02 15:20:16 +0200

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         </h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
